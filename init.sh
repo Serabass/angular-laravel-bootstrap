e89490fb9bfd385907c5dd24faabd255681939b6
Serabass <is_everything@mail.ru>
2019-06-27 01:52:49 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+#!/usr/bin/env bash
+
+PROJECTNAME=${PWD##*/}
+echo Crafting project $PROJECTNAME...
+
 rm .git
 git init
+yarn global add json
 yarn init
 yarn add @angular/cli lerna
 yarn lerna init
@@ -10,8 +16,24 @@ mv frontend packages
 cd packages/frontend
 yarn add @ngx-resource/core @ngx-resource/handler-ngx-http @uirouter/angular faker jquery lodash moment reflect-metadata
 yarn ng add ng-zorro-antd --i18n=ru_RU
-cd ../..
 mv proxy.config.json packages/frontend
-cd packages
+cat package.json | json -e "this.scripts.start += ' --proxy-config proxy.config.json --port 4201'" > package.json
+cd ../
 composer global require laravel/installer
 laravel new backend
+yarn
+cat package.json | json -e "this.scripts.art = 'php artisan'" > package.json
+cat package.json | json -e "this.scripts.seed = 'yarn art migrate:fresh && yarn art db:seed'" > package.json
+cat package.json | json -e "this.scripts.mf = 'yarn art migrate:fresh'" > package.json
+cat package.json | json -e "this.scripts.start = 'php artisan serve'" > package.json
+yarn art sleepingowl:install
+cd ../..
+cat package.json | json -e "if (this.scripts == undefined) this.scripts = {};" > package.json
+cat package.json | json -e "this.scripts.start = 'lerna run start --stream --parallel';" > package.json
+cat package.json | json -e "this.scripts.build = 'lerna run build --stream --parallel';" > package.json
+
+cd packages/frontend
+cat package.json | json -e "this.name = '@$PROJECTNAME/frontend'" > package.json
+
+cd ../backend
+cat package.json | json -e "this.name = '@$PROJECTNAME/backend'" > package.json
