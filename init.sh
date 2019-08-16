6bd7122fcbb7552485028812b4edc84a17b7a653
Serabass <is_everything@mail.ru>
2019-08-17 02:04:20 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -30,7 +30,9 @@ yarn
 cat package.json | json -e "this.scripts.art = 'php artisan'" > package.json
 cat package.json | json -e "this.scripts.seed = 'yarn art migrate:fresh && yarn art db:seed'" > package.json
 cat package.json | json -e "this.scripts.mf = 'yarn art migrate:fresh'" > package.json
-cat package.json | json -e "this.scripts.start = 'php artisan serve'" > package.json
+cat package.json | json -e "this.scripts.start = 'yarn art serve'" > package.json
+cat package.json | json -e "this.scripts.m = 'yarn art migrate'" > package.json
+cat package.json | json -e "this.scripts['ide-helper'] = 'yarn art ide-helper:eloquent && yarn art ide-helper:generate && yarn art ide-helper:meta && yarn art ide-helper:models'" > package.json
 yarn art sleepingowl:install
 cd ../..
 cat package.json | json -e "if (this.scripts == undefined) this.scripts = {};" > package.json
